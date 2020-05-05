2529235d67e2b31112049518692055a38d18e84a
eankeen <24364012+eankeen@users.noreply.github.com>
2020-05-04 23:58:10 -0700

    feat: get minimal example working

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>input</h1>
+    <textarea v-model="babelState.inputText" @keydown="babelTransform" cols="30" rows="10">let a = 4</textarea>
+    <h1>output</h1>
+    <button @click="babelTransform">transform</button>
+    <textarea v-model="babelState.transformedText" name="area" cols="30" rows="10"></textarea>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+/* eslint-disable */
+import { reactive, computed } from 'vue'
+import * as babel from '@babel/core'
+import p from '@babel/plugin-transform-modules-commonjs'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  setup() {
+    // start
+    const babelState = reactive({
+      inputText: "import eeee from 'p'",
+      transformedText: 'pre'
+    })
+
+    function babelTransform() {
+      (async () => {
+        const { code, map, ast } = await babel.transformAsync(babelState.inputText, {
+          plugins: [
+            p
+          ]
+        })
+        babelState.transformedText = code
+      })()
+    }
+
+    return {
+      babelState,
+      babelTransform
+    }
   }
 }
 </script>
