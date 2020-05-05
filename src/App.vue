4038f0d1a1b578baaf760deb1ccee4d28437237c
eankeen <24364012+eankeen@users.noreply.github.com>
2020-05-05 02:01:07 -0700

    feat: add error text

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,37 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <nav id="nav">
+      <router-link to="/">Home</router-link>
+      <select @change="choosePlugin" name="select" id="option">
+        <option value="plugin-transform-modules-commonjs">transform-modules-commonjs</option>
+        <option value="plugin-transform-spread">transform-spread</option>
+      </select>
+    </nav>
     <router-view/>
   </div>
 </template>
+
+<script>
+/* eslint-disable */
+export default {
+  name: 'App',
+  setup() {
+    function choosePlugin() {
+      console.log('change')
+    }
+
+    return {
+      choosePlugin
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="home">
-    <h1>input</h1>
-    <textarea v-model="babelState.inputText" @keydown="babelTransform" cols="30" rows="10">let a = 4</textarea>
-    <h1>output</h1>
+    <main class="input-output-parent">
+      <div class="input">
+        <textarea v-model="babelState.inputText" @keyup="babelTransform" name="input">let a = 4</textarea>
+      </div>
+      <div class="output">
+        <textarea v-model="babelState.transformedText" name="output"></textarea>
+      </div>
+    </main>
+    <h2>error</h2>
+    <p v-if="babelState.isErrorOnTransform">{{ babelState.errorText }}</p>
     <button @click="babelTransform">transform</button>
-    <textarea v-model="babelState.transformedText" name="area" cols="30" rows="10"></textarea>
   </div>
 </template>
 
@@ -13,6 +19,7 @@
 import { reactive, computed } from 'vue'
 import * as babel from '@babel/core'
 import p from '@babel/plugin-transform-modules-commonjs'
+import p2 from '@babel/plugin-transform-spread'
 
 export default {
   name: 'Home',
@@ -20,17 +27,28 @@ export default {
     // start
     const babelState = reactive({
       inputText: "import eeee from 'p'",
-      transformedText: 'pre'
+      transformedText: 'pre',
+      isErrorOnTransform: false,
+      errorText: null
     })
 
     function babelTransform() {
       (async () => {
-        const { code, map, ast } = await babel.transformAsync(babelState.inputText, {
-          plugins: [
-            p
-          ]
-        })
-        babelState.transformedText = code
+        try {
+          const { code, map, ast } = await babel.transformAsync(babelState.inputText, {
+            plugins: [
+              p, p2
+            ]
+          })
+          babelState.isErrorOnTransform = false
+          babelState.transformedText = code
+        } catch (err) {
+          // error with parsing
+          babelState.isErrorOnTransform = true
+          console.log('parsing error')
+          babelState.errorText = err
+        }
+
       })()
     }
 
@@ -41,3 +59,24 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.input-output-parent {
+  height: 200px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+
+.input {
+
+}
+
+.output {
+
+}
+
+textarea {
+  width: 100%;
+  height: 100%;
+}
+</style>
